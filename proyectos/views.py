5b5cf3ac436f7c107ff2f95d3386da151edc6124
renato <renatocespedesf@gmail.com>
2022-12-10 18:41:33 -0500

    vistas para proyectos

--- proyectos/views.py
+++ proyectos/views.py
@@ -11,8 +11,47 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 
 from django.urls import reverse
-# Create your views here.
 
+
+class ProjectList(ListView):
+    model = ProjectModel
+    template_name = "profile.html"
+    data = ProjectModel.objects.all()    
+
+class ElementProjecto(View):
+    template='element.html'
+    context={}
+    def get(self,request,id,usuario):
+        project=ProjectModel.objects.filter(id=id)
+        self.context["project"]=project[0]
+        return render(request,self.template,self.context)
+
+class Editelement(View):
+    template='edition.html'
+    context={}
+    def get(self,request,id,usuario):
+        elem=ProjectModel.objects.filter(id=id)
+        form=ProjectForm()
+        self.context['form']=form
+        self.context['elem']=elem[0]
+        self.context['usuario']=usuario
+        return render(request,self.template,self.context)
+    def post(self,request,id,usuario):
+        element=ProjectModel.objects.get(id=id)
+        # element=elem[0]
+        form_elem=ProjectForm(request.POST)
+        print("-----------------------")
+        print(form_elem.is_valid())
+        print("-----------------------")
+        if form_elem.is_valid():
+            element.foto=form_elem.cleaned_data['foto']
+            element.titulo=form_elem.cleaned_data['titulo']
+            element.descripcion=form_elem.cleaned_data['descripcion']
+            element.tags=form_elem.cleaned_data['tags']
+            element.repo_url=form_elem.cleaned_data['repo_url']
+            element.save()
+            return redirect(reverse('element',kwargs={'id':id,'usuario':usuario}))
+        return HttpResponse('No se llego a validar')
 
 class RegisterProject(LoginRequiredMixin,View):
     login_url = '/login/'
@@ -33,5 +72,10 @@ class RegisterProject(LoginRequiredMixin,View):
         
 class Home(View):
     def get(self,request):
-    # genero=request.GET['genero']
         return render(request, 'index.html', {})
+
+def delete(request, id,usuario):
+    data = ProjectModel.objects.get(id=id) 
+    data.delete()
+    # message.suc
+    return redirect(reverse('profile',kwargs={'usuario':usuario}))
