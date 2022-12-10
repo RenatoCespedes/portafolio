c899c097a206ee041c9d33ca8b774bbe405929a8
renato <renatocespedesf@gmail.com>
2022-12-10 18:34:51 -0500

    modelo contacto

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.db.models.fields import CharField,URLField,EmailField,TextField
+from django.db.models.fields.files import ImageField
 # Create your models here.
+class Contacto(models.Model):
+    nombre=CharField(max_length=50)
+    correo=EmailField()
+    tema=CharField(max_length=100)
+    mensaje=TextField()
