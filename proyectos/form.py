d827f8a0330866c5a760b90d6cab6bb64f78f0e2
renato <renatocespedesf@gmail.com>
2022-12-10 18:33:28 -0500

    formulario proyectos

--- proyectos/form.py
+++ proyectos/form.py
@@ -1,10 +1,10 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+
 
 
 class ProjectForm(forms.Form):
     foto= forms.CharField(max_length=800)
     titulo=forms.CharField(max_length=100)
     descripcion=forms.CharField(max_length=1000)
-    tags=forms.CharField(max_length=500)
-    repo_url=forms.CharField(max_length=800)
+    tags=forms.ImageField()
+    repo_url=forms.URLField(max_length=800)
