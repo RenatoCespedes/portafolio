6c6394fdd215e8ced0448f4b77447d2ea5c11a15
renato <renatocespedesf@gmail.com>
2022-12-10 14:50:01 -0500

    Modelo proyecto

--- proyectos/models.py
+++ proyectos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.db.models.fields import CharField,URLField,EmailField,TextField
 # Create your models here.
+class ProjectModel(models.Model):
+    foto= CharField(max_length=100)
+    titulo=CharField(max_length=100)
+    descripcion=CharField(max_length=1000)
+    tags=CharField(max_length=500)
+    repo_url=URLField(blank=True)
