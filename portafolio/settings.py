0dfdf63d30b9e57ecea19da9546eaf630b58b424
renato <renatocespedesf@gmail.com>
2022-12-11 21:28:31 -0500

    new config

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.app.UsersConfig',
-    'proyectos.app.ProyectosConfig',
+    'proyectos.apps.ProyectosConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'livereload',
     'django.contrib.staticfiles',
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'portafolio.urls'
@@ -129,6 +131,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 REDIRECT_FIELD_NAME='/login'
 LOGOUT_REDIRECT_URL='/'
 ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = True
