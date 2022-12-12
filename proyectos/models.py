ae3319e07e07ac995cf621b038a3e744e03a54e9
renato <renatocespedesf@gmail.com>
2022-12-11 21:29:41 -0500

    lectura de image en profile

--- proyectos/models.py
+++ proyectos/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.db.models.fields import CharField,URLField,EmailField,TextField
 from django.db.models.fields.files import ImageField
+from django.contrib.auth.models import User
 # Create your models here.
 class ProjectModel(models.Model):
-    foto= ImageField()
+    foto= ImageField(upload_to="images/")
     titulo=CharField(max_length=100)
     descripcion=CharField(max_length=1000)
     tags=CharField(max_length=500)
     repo_url=URLField(blank=True)
+    usuarios=models.ForeignKey(User,on_delete=models.CASCADE)
